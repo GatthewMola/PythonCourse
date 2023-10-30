da9d98f29bf49359a222ebb234f6d784a10542ab
GatthewMola <mattgola@outlook.com>
2023-10-30 18:50:08 -0400

    Hang on

--- Geometry_Game_Code.py
+++ Geometry_Game_Code.py
@@ -4,10 +4,7 @@ listy = [6,7]
 
 stringy = "Hi"
 
+#This azores map object can also be saved as an html file (.save("output.html"))
 import folium
 azores = folium.folium.Map(location = (38, -27),
                     zoom_start = 6)
-
-#This azores map object can also be saved as an html file
-azores = folium.folium.Map(location = (38, -27),
-                    zoom_start = 6).save("output.html")
