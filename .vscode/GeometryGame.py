c438a7cd1235dce7f570538ba3d348e30a3e176e
GatthewMola <mattgola@outlook.com>
2023-11-02 11:10:55 -0400

    Created new class and method in GeometryGame and udpadted TurtlePractice.

--- .vscode/GeometryGame.py
+++ .vscode/GeometryGame.py
@@ -1,45 +1,68 @@
+from random import randint
+
+import turtle
+
 class Point:
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
     
-    def falls_in_rectangle(self, rectangle):
-        if rectangle.lowleft.x < self.x < rectangle.upright.x \
-        and rectangle.lowleft.y < self.y < rectangle.upright.y:
+    def falls_in_rectangle(self, gui_rectangle):
+        if gui_rectangle.lowleft.x < self.x < gui_rectangle.upright.x \
+        and gui_rectangle.lowleft.y < self.y < gui_rectangle.upright.y:
             return True
         else:
             return False
 
 class Rectangle:
 
-    def __init__(self, lowleft, upright):
-        self.lowleft = lowleft
-        self.upright = upright
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
     
     def rectangle_area(self):
-        return (self.upright.x - self.lowleft.x) * \
-            (self.upright.y - self.lowleft.y)
+        return (self.point2.x - self.point1.x) * \
+            (self.point2.y - self.point1.y)
 
-from random import randint
 
-rectangle = Rectangle(
-    Point(randint(0, 9), randint(0, 9)),
-    Point(randint(10, 19), randint(10, 19))
-)
+class GuiRectangle(Rectangle):
+
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.point1.x, self.point1.y)
+
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+
+        turtle.done()
+
+gui_rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)),
+    Point(randint(10, 400), randint(10, 400)))
+
+myturtle = turtle.Turtle()
+
+gui_rectangle.draw(canvas=myturtle)
+
 
 print("Rectangle Coordinates: ",
-    rectangle.lowleft.x, ",",
-    rectangle.lowleft.y, "and",
-    rectangle.upright.x, ",",
-    rectangle.upright.y)
+    gui_rectangle.lowleft.x, ",",
+    gui_rectangle.lowleft.y, "and",
+    gui_rectangle.upright.x, ",",
+    gui_rectangle.upright.y)
 
 user_point = Point(float(input("Guess X: ")),
                 float(input("Guess Y: ")))
 
-user_area = (float(input("Guess rectangle area: ")))
+user_area = (float(input("Guess gui_rectangle area: ")))
 
-print("Your point was inside rectangle: ",
-user_point.falls_in_rectangle(rectangle))
+print("Your point was inside gui_rectangle: ",
+user_point.falls_in_rectangle(gui_rectangle))
 
-print("Your area was off by: ", rectangle.rectangle_area() - user_area)
+print("Your area was off by: ", gui_rectangle.rectangle_area() - user_area)
--- .vscode/TurtlePractice.py
+++ .vscode/TurtlePractice.py
@@ -2,6 +2,17 @@ import turtle
 
 myturtle = turtle.Turtle()
 
+# Go to a certain coordinate (50 pixels on x axis, 75 on y)
+myturtle.pen() # Picks up the pen so that it does not draw the "goto" line
+myturtle.goto(50, 75)
+
+myturtle.pendown() # Puts the pen back down on the canvas
+myturtle.forward(100) # Move 100 pixels
+myturtle.left(90) # Turn left 90 degrees
+myturtle.forward(200)
+myturtle.left(90)
 myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
 
 turtle.done()
