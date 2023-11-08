0509cd9262ced688b075d44b850df68981b10a28
GatthewMola <mattgola@outlook.com>
2023-11-08 10:32:34 -0500

    Committing several module notes and added code to app.

--- .vscode/FlatBillSharing_App/FlatBillSharing.py
+++ .vscode/FlatBillSharing_App/FlatBillSharing.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
     Object that contains data about a bill, such as total
@@ -36,7 +38,34 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add Icon
+        pdf.image("house.png", w=30, h=30)
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+        # Insert Period and Month labels and values
+        pdf.cell(w=100, h=40, txt="Period:", border=1)
+        pdf.cell(w=175, h=40, txt=bill.period, border=1, ln=1)
+
+
+        # Insert name and due amount for the first flatmate
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=175, h=40, txt=flatmate1_pay, border=1, ln=1)
+
+        # Insert name and due amount for the second flatmate
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=175, h=40, txt=flatmate2_pay, border=1, ln=1)
+
+        pdf.output(self.filename)
 
 
 bill = Bill(amount = 120, period = "November 2023")
@@ -45,3 +74,6 @@ syd = Flatmates(name='Syd', days_in_flat=25)
 
 print("Matt pays: ", matt.pays(bill=bill, flatmate2=syd))
 print("Syd pays: ", syd.pays(bill, flatmate2=matt))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=matt, flatmate2=syd, bill=bill)
