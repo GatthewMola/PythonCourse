dcdfd8c5f235e5ae4e20be6c92abc7f5541b1335
GatthewMola <mattgola@outlook.com>
2023-11-17 10:53:59 -0500

    Saving changes to Webcam App before closing window.

--- .vscode/WebcamPhotoShare_App/MainApp/WebcamPhotoShare_Main.py
+++ .vscode/WebcamPhotoShare_App/MainApp/WebcamPhotoShare_Main.py
@@ -1,16 +1,50 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
+import urllib.request
 
-# As long as the file we are loading is in the same directory as the MainApp file, we only need to input the name of the file alone.
-Builder.load_file('frontend.kv')
+Builder.load_string("""
+<FirstScreen>:
+    GridLayout:
+        cols: 1
+        padding: 10
+        spacing: 10
+        Image:
+            id: img
+            size_hint_y: 0.8
+        TextInput:
+            id: user_query
+            size_hint_y: 0.1
+        Button:
+            text: 'Search Image'
+            size_hint_y: 0.1
+            on_press: root.search_image()
+
+<RootWidget>:
+    FirstScreen:
+        id: first_screen
+        name: 'first_screen'
+""")
 
 # Because we are only utilizing one screen at this point in our app, we only require one screen class.
 class FirstScreen(Screen):
 
     # When the user uses the search bar to search for an image, we need a method that will fulfill the request.
     def search_image(self):
-        pass
+        # Allows for users to query from the search bar.
+        query = self.manager.current_screen.ids.user_query.text
+        # Creates an instance for the user query to run through wikipedia URLS (essentially a wikipedia search).
+        page = wikipedia.page(query)
+        # Declaring [0] forces the function to run on the first URL in the search.
+        image_link = page.images[0]
+        # Download the queried image.
+        req = requests.get(image_link)
+        # Create file from the download.
+        urllib.request.urlretrieve(image_link, 'query_image.jpg')
+        # Set the image into the Image widget
+        self.manager.current_screen.ids.img.source = 'query_image.jpg'
 
 class RootWidget(ScreenManager):
     pass
