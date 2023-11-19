22b6085e037b1333374e03a5a31fdf3b55deb554
GatthewMola <mattgola@outlook.com>
2023-11-19 11:22:48 -0500

    Completed Kivy introduction.

--- .vscode/WebcamPhotoShare_App/MainApp/WebcamPhotoShare_Main.py
+++ .vscode/WebcamPhotoShare_App/MainApp/WebcamPhotoShare_Main.py
@@ -29,22 +29,16 @@ Builder.load_string("""
 """)
 
 # Because we are only utilizing one screen at this point in our app, we only require one screen class.
+# Comments for individual method actions found in Section17 Notebook
 class FirstScreen(Screen):
 
-    # When the user uses the search bar to search for an image, we need a method that will fulfill the request.
     def search_image(self):
-        # Allows for users to query from the search bar.
         query = self.manager.current_screen.ids.user_query.text
-        # Creates an instance for the user query to run through wikipedia URLS (essentially a wikipedia search).
         page = wikipedia.page(query)
-        # Declaring [0] forces the function to run on the first URL in the search.
         image_link = page.images[0]
-        # Download the queried image.
-        req = requests.get(image_link)
-        # Create file from the download.
         urllib.request.urlretrieve(image_link, 'query_image.jpg')
-        # Set the image into the Image widget
         self.manager.current_screen.ids.img.source = 'query_image.jpg'
+
 
 class RootWidget(ScreenManager):
     pass
