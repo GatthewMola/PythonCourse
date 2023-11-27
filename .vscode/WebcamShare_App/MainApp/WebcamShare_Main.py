c407fc6c372252d8fee1d6f8289f60ee7a806a6c
GatthewMola <mattgola@outlook.com>
2023-11-27 10:03:41 -0500

    Commit

--- .vscode/WebcamShare_App/MainApp/WebcamShare_Main.py
+++ .vscode/WebcamShare_App/MainApp/WebcamShare_Main.py
@@ -6,7 +6,27 @@ Builder.load_file('frontend.kv')
 
 
 class CameraScreen(Screen):
-    pass
+    
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def capture(self):
+        pass
+
+
+class FileSharer: # I need to go back into the last module of the Flatmates Bill Sharing App to find this code explanation.
+
+    def __init__(self, filepath, api_key="AViVqp7suSQWWEdrl6hf9z"):
+        self.filepath = filepath
+        self.api_key = api_key
+
+    # def share(self):
+    #     client = Client(self.api_key)
+    #     new_filelink = client.upload(filepath=self.filepath)
+    #     return new_filelink.url
 
 
 class ImageScren(Screen):
